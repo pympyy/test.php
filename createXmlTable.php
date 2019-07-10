217ad704e156d18d1ad5581dfff0626c621c25a3
pympyy <40663840+pympyy@users.noreply.github.com>
2019-07-10 16:07:07 +0300

    origin
    exit to work

--- createXmlTable.php
+++ createXmlTable.php
@@ -3,8 +3,28 @@
 	if (isset($_POST['table_name'])) {
 		
 		$table_name = $_POST['table_name'];
-		$mysqli = new mysqli ("localhost", "root", "", "xmlBase");
-		$mysqli->query ("CREATE TABLE `xmlbase`.`$table_name` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+
+
+		// $target_dir = "uploads/";
+		// $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		// $uploadOk = 1;
+		// $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		// // Check if image file is a actual image or fake image
+		// if(isset($_POST["submit"])) {
+		//     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		//     if($check !== false) {
+		//         echo "File is an image - " . $check["mime"] . ".";
+		//         $uploadOk = 1;
+		//     } else {
+		//         echo "File is not an image.";
+		//         $uploadOk = 0;
+		//     }
+		// }
+
+
+		// $mysqli = new mysqli ("localhost", "root", "", "xmlBase");
+		// $mysqli->query ("CREATE TABLE `xmlbase`.`$table_name` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+		echo "создана таблица с именем $table_name";
 
 	}
 	
--- index.php
+++ index.php
@@ -5,16 +5,16 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-	<form style="width: 400px;margin:0 auto;" name="createXmlTable" method="post" action="createXmlTable.php">
+	<form style="width: 400px;margin:0 auto;" name="createXmlTable" method="post" action="createXmlTable.php" enctype="multipart/form-data">
 	  <div class="form-group">
 	    <label for="table_name">Table name</label>
 	    <input type="text" class="form-control" name="table_name" id="table_name" placeholder="Enter table name">
 	    <small id="nameHelp" class="form-text text-muted">Enter name for your XML table in MySQL</small>
 	  </div>
-	  <!-- <div class="form-group">
-	    <label for="file">Choice u XML file</label>
-	    <input type="file" class="form-control-file" id="file">
-	  </div> -->
+	  <div class="form-group">
+	    <label for="fileToUpload">Choice u XML file</label>
+	    <input type="file" class="form-control-file" name="fileToUpload" id="fileToUpload">
+	  </div>
 	  <button type="submit" class="btn btn-primary">Submit</button>
 	</form>
 </body>
