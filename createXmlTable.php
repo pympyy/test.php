9f0449fb07ca599bdb4b22e001b3372ff275f4b6
pympyy <40663840+pympyy@users.noreply.github.com>
2019-07-11 11:46:31 +0300

    origin
    xml to sql

--- createXmlTable.php
+++ createXmlTable.php
@@ -1,33 +1,88 @@
 <?php
 
 	if (isset($_POST['table_name'])) {
-		
-		$table_name = $_POST['table_name'];
+		if ($_FILES['fileToUpload']['type'] == "text/xml") {
 
+			$uploaddir = __DIR__."\uploads\\";
+			$uploadfile = $uploaddir . basename($_FILES['fileToUpload']['name']);
 
-		// $target_dir = "uploads/";
-		// $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-		// $uploadOk = 1;
-		// $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-		// // Check if image file is a actual image or fake image
-		// if(isset($_POST["submit"])) {
-		//     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-		//     if($check !== false) {
-		//         echo "File is an image - " . $check["mime"] . ".";
-		//         $uploadOk = 1;
-		//     } else {
-		//         echo "File is not an image.";
-		//         $uploadOk = 0;
-		//     }
-		// }
+			//move_uploaded_file — Перемещает загруженный файл в новое место
+			if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile)) {
+			    // echo "Файл корректен и был успешно загружен.\n";
 
+				$table_name = $_POST['table_name'];
 
-		// $mysqli = new mysqli ("localhost", "root", "", "xmlBase");
-		// $mysqli->query ("CREATE TABLE `xmlbase`.`$table_name` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
-		echo "создана таблица с именем $table_name";
+				$mysqli = new mysqli ("localhost", "root", "", "xmlBase");
 
+				if ($mysqli->connect_errno) {
+				    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+				}else{
+
+					$mysqli->query ("CREATE TABLE `xmlbase`.`$table_name` (`id` INT NOT NULL AUTO_INCREMENT,`Name` varchar(255) NULL,`Razdel` varchar(255) NULL,`global_id` int(11) NULL,`Idx` varchar(255) NULL,`Kod` varchar(255) NULL,`Nomdescr` TEXT NULL,PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+
+					$xml = simplexml_load_file($uploadfile) or die("Error: Cannot create object");
+
+					$affectedRow = 0;
+
+					foreach ($xml->children() as $i => $row) {
+						    $name = $row->Name;
+						    $razdel = $row->Razdel;
+						    $global_id = $row->global_id;
+						    $idx = $row->Idx;
+						    $kod = $row->Kod;
+						    $nomdescr = $row->Nomdescr;
+					    
+					    // $sql = "INSERT INTO $table_name(Name,Razdel,global_id,Idx,Kod,Nomdescr) VALUES ('" . $name . "','" . $razdel . "','" . $global_id . "','" . $idx . "','" . $kod . "','" . $nomdescr . "')";
+					    if ($sql = $mysqli->prepare("INSERT INTO $table_name(Name,Razdel,global_id,Idx,Kod,Nomdescr) VALUES (?,?,?,?,?,?)")) {
+					    $sql = $mysqli->prepare("
+					    	INSERT INTO $table_name(Name,Razdel,global_id,Idx,Kod,Nomdescr) 
+					    	VALUES (?,?,?,?,?,?)
+				    	");
+						}else{
+							// логировать ошибку
+							echo "Не удалось подготовить запрос: (" . $mysqli->errno . ") " . $mysqli->error;
+						}
+				    	if ($sql->bind_param("ssisss", $name, $razdel, $global_id, $idx, $kod, $nomdescr)) {
+					    	$sql->bind_param("ssisss", $name, $razdel, $global_id, $idx, $kod, $nomdescr);
+					    	$sql->execute();
+					    	$sql->close();
+				    	}else{
+				    		// логировать ошибку
+				    		echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+				    	}
+
+					    // $result = mysqli_query($mysqli, $sql);
+
+					 //    if (! empty($result)) {
+					 //        $affectedRow ++;
+					 //    } else {
+					 //        $error_message = mysqli_error($conn) . "\n";
+					 //    }
+
+					 //    if ($affectedRow > 0) {
+						//     $message = $affectedRow . " records inserted";
+						// } else {
+						//     $message = "No records inserted";
+						// }
+
+					}
+				}
+
+			} else {
+				// логировать ошибку
+			    echo "Возможная атака с помощью файловой загрузки!\n";
+			}
+		}else{
+			// логировать ошибку по типу файла
+		}
+	}else{
+		// логировать ошибку по имени
 	}
-	
 
 
 ?>
+
+<div><?php  echo $message; ?></div>
+<?php if (! empty($error_message)) { ?>
+<div><?php echo nl2br($error_message); ?></div>
+<?php } ?>
